77e27f85d0f5dc6569fe70a41402d7cfebc48d38
cutymeoRBLX <60260512+cutymeoRBLX@users.noreply.github.com>
2022-01-02 19:19:40 +0700

    Update osu_to_lua.js

--- osu_to_lua.js
+++ osu_to_lua.js
@@ -128,7 +128,7 @@ module.export("osu_to_lua", function(osu_file_contents) {
 		var track = hitobj_x_to_track_number(itr.position[0]);
 		var beat = (itr.startTime / 1000) / (60 / beatmap.bpmMax);
 		var duration = itr.duration;
-		if (!isNaN(duration)) duration /= 100;
+		if (!isNaN(duration)) duration /= 1000;
 		else duration = 0;
 		append_to_output(format("			{Key=%d,Beat=%d,Length=%d},", track, beat, duration))
 		/*
