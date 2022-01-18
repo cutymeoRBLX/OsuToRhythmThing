6189011ee7358c150be3e4ab99bf8e107b1b57c1
cutymeoRBLX <60260512+cutymeoRBLX@users.noreply.github.com>
2022-01-18 17:58:50 +0700

    Fixed properties.

--- osu_to_lua.js
+++ osu_to_lua.js
@@ -154,7 +154,7 @@ module.export("osu_to_lua", function(osu_file_contents) {
 	
 	for (var i = 0; i < beatmap.timingPoints.length; i++) {
 		var itr = beatmap.timingPoints[i];
-		var isSV = itr.inherited == 0 || itr.ms_per_beat < 0;
+		var isSV = !itr.timingChange || itr.beatLength < 0;
 		if (isSV)
 			SVsPoints.push(itr);
 		else
@@ -174,7 +174,7 @@ module.export("osu_to_lua", function(osu_file_contents) {
 		append_to_output("	SliderVelocities = {");
 		for (var i = 0; i < SVsPoints.length; i++) {
 			var itr = SVsPoints[i];
-			append_to_output(format("\t[%d] = { StartTime = %d; Multiplier = %d; };",i+1, itr.offset,clamp((-100 / itr.ms_per_beat), 0.1, 10)))
+			append_to_output(format("\t[%d] = { StartTime = %d; Multiplier = %d; };",i+1, itr.offset,clamp((-100 / itr.beatLength), 0.1, 10)))
 		}
 		append_to_output("	},");
 	}
