c69ea4b862d861f49d748861393c45600479e56c
cutymeoRBLX <60260512+cutymeoRBLX@users.noreply.github.com>
2022-01-19 08:11:35 +0700

    Update osu_to_lua.js

--- osu_to_lua.js
+++ osu_to_lua.js
@@ -174,7 +174,7 @@ module.export("osu_to_lua", function(osu_file_contents) {
 		append_to_output("	SliderVelocities = {");
 		for (var i = 0; i < SVsPoints.length; i++) {
 			var itr = SVsPoints[i];
-			append_to_output(format("\t[%d] = { StartTime = %d; Multiplier = %d; };",i+1, itr.offset/1000, clamp((-100 / itr.beatLength), 0.1f, 10)));
+			append_to_output(format("\t[%d] = { StartTime = %d; Multiplier = %d; };",i+1, itr.offset/1000, clamp((-100 / itr.beatLength), 0.1, 10)));
 		}
 		append_to_output("	},");
 	}
